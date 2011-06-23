91d1d4a901cfc1126561b2657c20fa496255c508
Fabio Lessa <flessa@gmail.com>
2011-06-23 21:40:15 +0530

    found a better way to read metadata information, refactored the way we build the war file name

--- scripts/Dist.groovy
+++ scripts/Dist.groovy
@@ -1,22 +1,18 @@
 import org.codehaus.groovy.grails.commons.ApplicationHolder
 
-includeTargets << grailsScript("Init")
+includeTargets << grailsScript("_GrailsSettings")
 includeTargets << grailsScript("_GrailsWar")
+
+def winstoneDir = "target/winstone"
+def winstoneManifest = "${winstoneDir}/META-INF/MANIFEST.MF"
+def warFileName = "target/${metadata["app.name"]}-${metadata["app.version"]}.war"
 
 target(main: "The description of the script goes here!") {
     depends(war)
-    def winstoneDir = "target/winstone"
-    def winstoneManifest = "${winstoneDir}/META-INF/MANIFEST.MF"
-    def warFileName = "target/${appMetadata("app.name")}-${appMetadata("app.version")}.war"
-
     ant.mkdir(dir: winstoneDir)
     ant.unzip(src: "lib/winstone-0.9.10.jar", dest: winstoneDir)
     ant.copy(file: warFileName, tofile: "${winstoneDir}/embedded.war")
     ant.jar(destfile: "target/frontlinesms-legal.jar", basedir: winstoneDir, manifest: winstoneManifest)
-}
-
-String appMetadata(String property) {
-    ApplicationHolder.application.metadata[property]
 }
 
 setDefaultTarget(main)
