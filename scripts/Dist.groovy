3148a87aa276875db5b62a236e6628a6f4ded56e
Fabio Lessa <flessa@gmail.com>
2011-06-23 17:28:06 +0530

    fabio/eric/shruti: refactored dist build target

--- scripts/Dist.groovy
+++ scripts/Dist.groovy
@@ -1,14 +1,22 @@
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+
 includeTargets << grailsScript("Init")
 includeTargets << grailsScript("_GrailsWar")
 
-def winstoneDir = "target/winstone"
-
 target(main: "The description of the script goes here!") {
     depends(war)
+    def winstoneDir = "target/winstone"
+    def winstoneManifest = "${winstoneDir}/META-INF/MANIFEST.MF"
+    def warFileName = "target/${appMetadata("app.name")}-${appMetadata("app.version")}.war"
+
     ant.mkdir(dir: winstoneDir)
     ant.unzip(src: "lib/winstone-0.9.10.jar", dest: winstoneDir)
-    ant.copy(file: "target/frontlinesms-legal-0.1.war", tofile: "${winstoneDir}/embedded.war")
-    ant.jar(destfile: "target/frontlinesms-legal.jar", basedir: winstoneDir, manifest: "${winstoneDir}/META-INF/MANIFEST.MF")
+    ant.copy(file: warFileName, tofile: "${winstoneDir}/embedded.war")
+    ant.jar(destfile: "target/frontlinesms-legal.jar", basedir: winstoneDir, manifest: winstoneManifest)
+}
+
+String appMetadata(String property) {
+    ApplicationHolder.application.metadata[property]
 }
 
 setDefaultTarget(main)
