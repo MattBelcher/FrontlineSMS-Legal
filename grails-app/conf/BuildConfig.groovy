f08ed2db968d865943f701d4d8f50ec7b2677742
Fabio Lessa <flessa@gmail.com>
2011-06-28 12:35:19 +0530

    fabio/paul: removed build.groovy. we learned the hard way that targets have to be called individually

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+
 grails.project.dependency.resolution = {
     inherits("global") {
     }
@@ -22,6 +23,7 @@ grails.project.dependency.resolution = {
         test 'org.seleniumhq.selenium:selenium-ie-driver:latest.release'
         test 'org.spockframework:spock-core:0.5-groovy-1.7'
     }
+
 }
 
 coverage {
--- scripts/UpdateCore.groovy
+++ scripts/UpdateCore.groovy
@@ -2,9 +2,10 @@ includeTargets << grailsScript("_GrailsProxy")
 includeTargets << grailsScript("_GrailsPlugins")
 
 target(updateCore: "Update FrontlineSMS Core plugin") {
-    def url = new URL("http://10.10.5.48:8080/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/target/frontlinesms-core.zip")
-    if (connectionAvailable(url)) {
-        doInstallPluginFromURL(url)
+    def url = "http://10.10.5.48:8080/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"
+    if (connectionAvailable(new URL(url))) {
+        args = url
+        installPlugin()
     } else {
         echo "WARNING: Could not load the latest version of FrontlineSMS Core"
     }
