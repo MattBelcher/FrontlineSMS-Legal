cf5206ecb8a5e3a7665f78332aa3187796502aec
Afmrak <enmrak@gmail.com>
2011-06-30 10:17:24 +0530

    Mrak, Lessa: added SASS to BootStrap to monitor for SASS changes and generate CSS files on the fly

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.current == Environment.DEVELOPMENT) {
+            def ant = new AntBuilder()
+            ant.java(jar: "lib/java/jruby-complete-1.6.2.jar", fork: "true", spawn: "true") {
+                def command = "-rlib/ruby/sass.jar "
+                command += "-e \"require 'rubygems';load Gem.bin_path('sass', 'sass', '3.1.3')\" "
+                command += "-- --watch src/sass:web-app/css"
+                arg(line: command)
+            }
+        }
     }
     def destroy = {
     }
