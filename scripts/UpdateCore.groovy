c7888b1bb0e8133ecb16d31020340e35b609c05a
Fabio Lessa <flessa@gmail.com>
2011-07-04 16:32:00 +0530

    fabio/anshul: updated the ci server's address on the update-core script

--- scripts/UpdateCore.groovy
+++ scripts/UpdateCore.groovy
@@ -2,7 +2,7 @@ includeTargets << grailsScript("_GrailsProxy")
 includeTargets << grailsScript("_GrailsPlugins")
 
 target(updateCore: "Update FrontlineSMS Core plugin") {
-    def url = "http://10.10.5.48:8080/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"
+    def url = "http://221.134.198.6/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"
     if (connectionAvailable(new URL(url))) {
         args = url
         installPlugin()
