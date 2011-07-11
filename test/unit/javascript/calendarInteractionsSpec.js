4467b6b3997bded719d63eb15af4db5efec1145e
Devaradhan <devaradhan@gmail.com>
2011-07-11 15:20:37 +0530

    deva/paul resolving build failure -reverting calendarInteractionSpec to prev successful build

--- test/unit/javascript/calendarInteractionsSpec.js
+++ test/unit/javascript/calendarInteractionsSpec.js
@@ -1,87 +1,12 @@
 describe('calculateScheduleHeight', function () {
-    beforeEach(function () {
-        $('#fixture').remove();
-        $("<div id=\"header\"></div><div id=\"schedule\"></div>").appendTo('#fixtures')
-        $('head').append("<script type=\"text/javascript\" src=\"/js/fullcalendar.js\"></script>");
-        $('body').append("<div id=\"view-event\" title=\"Event Details\">\
-    <label>Title :</label>\
-    <label name=\"eventTitle\" id=\"event-title\"></label><br/>\
-    <label>Date :</label>\
-    <label name=\"eventDate\" id=\"event-date\"></label><br/>\
-    <input type=\"button\" id=\"delete-event\" value=\"Delete\"/>\
-    <label name=\"eventId\" id=\"event-id\"></label>");
-
-        $("#header").css("height", 5);
-        $("#schedule").css("padding", 10);
-        $('#schedule').fullCalendar({
-                    events: [
-                        {
-                            title: 'All Day Event',
-                            start: new Date()
-                        }
-                    ]
-                });
-
-        frontlinesms.calendarInteractions();
-        frontlinesms.eventDetails();
-        $('#event-id').hide();
-    });
-
     it('uses the window height, header height and top/bottom padding to ', function () {
+        $("<div id=\"header\"></div><div id=\"schedule\"></div>").appendTo("#fixtures");
         $("#header").css("height", 5);
         $("#schedule").css("padding", 10);
         var windowHeight = 500;
 
         expect(frontlinesms.calculateScheduleHeight(windowHeight)).toEqual(windowHeight - 10 - 10 - 5);
     });
-
-    it('show pop-up dialog on clicking an event', function() {
-
-        $('body').append("<div id=\"header\"></div><div id=\"schedule\"></div>");
-        $("span[class = 'fc-event-title']").first().click();
-        expect($('#view-event').is(':visible'));
-    });
-    it('show cancel confirm dialog on clicking delete button', function() {
-
-        span[class = 'fc-event-title'].first().click();
-        oldEventSize = $("span[class='fc-event-title']'").size()
-        $('#delete-event').click()
-        expect($('#event-cancel-dialog').is(':visible'));
-
-        $('#cancel-confirm-no').click()
-        expect($('#view-event').is(':visible'));
-        expect(oldEventSize == $("span[class='fc-event-title']'").size())
-
-        $('#delete-event').click()
-        $('#cancel-confirm-yes').click()
-        expect($('#view-event').is(':visible'));
-        expect(oldEventSize == $("span[class='fc-event-title']'").size() + 1)
-
-
-    });
-
-    it('should not delete event on No button in cancel confirm dialog'),function() {
-        span[class = 'fc-event-title'].first().click();
-        oldEventSize = $("span[class='fc-event-title']'").size()
-        $('#delete-event').click()
-        expect($('#event-cancel-dialog').is(':visible'));
-
-        $('#cancel-confirm-no').click()
-        expect($('#view-event').is(':visible'));
-        expect(oldEventSize == $("span[class='fc-event-title']'").size())
-
-    }
-
-    it('should delete event on No button in cancel confirm dialog'),function() {
-        span[class = 'fc-event-title'].first().click();
-        oldEventSize = $("span[class='fc-event-title']'").size()
-        $('#delete-event').click()
-        expect($('#event-cancel-dialog').is(':visible'));
-
-        $('#delete-event').click()
-        $('#cancel-confirm-yes').click()
-        expect($('#view-event').is(':visible'));
-        expect(oldEventSize == $("span[class='fc-event-title']'").size() + 1)
-
-    }
 });
+
+
