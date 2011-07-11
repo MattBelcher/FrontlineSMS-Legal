b5eb369b3708d7f600ebfe266c2a95f18a3c3559
Devaradhan <devaradhan@gmail.com>
2011-07-11 13:45:38 +0530

    deva: refactored deleteEventSpec

--- test/functional/frontlinesms/legal/events/DeleteEventSpec.groovy
+++ test/functional/frontlinesms/legal/events/DeleteEventSpec.groovy
@@ -3,41 +3,35 @@ package frontlinesms.legal.events
 import frontlinesms.legal.functionaltests.FrontlinesmsLegalGebSpec
 import frontlinesms.legal.functionaltests.pages.events.NewEventPage
 import frontlinesms.legal.functionaltests.pages.schedule.SchedulePage
+import spock.lang.Ignore
 
 class DeleteEventSpec extends FrontlinesmsLegalGebSpec {
 
     def 'should delete event on clicking delete button on the pop-up dialog with event details'() {
         given:
+            createEvent("Test Event")
+       //     createEvent("Test Event1")
+            to SchedulePage , "index"
+        when:
+        def oldEventListSize = events().size();
+
+        events()[0].click()
+        deleteEvent()
+
+        def newEventListSize=isEventListSize()
+
+        then:
+        oldEventListSize == newEventListSize + 1;
+    }
+
+    def createEvent(title) {
         to NewEventPage
-        eventTitle = "Test Event"
+        eventTitle = title
         dateFieldSelected = new Date().format("MMMM d, yyyy")
         startTimeField = "08:09PM"
         endTimeField = "08:56AM"
         save.click()
-
-        when:
-        to SchedulePage, "index"
-        def eventList = events.collect {it}
-        for (def event in eventList) {
-            if (event.text().contains("Test Event")) {
-                event.click()
-                break;
-            }
-        }
-        def oldEventListSize = events.size();
-        events[0].click()
-        deleteEventButton.click()
-        def newEventListSize = events.size();
-
-        then:
-        oldEventListSize == events.size() + 1;
-
-        and:
-
-        when:
-        to SchedulePage , "index"
-
-        then:
-        newEventListSize == events.size()
     }
+
+
 }
--- test/functional/frontlinesms/legal/functionaltests/pages/schedule/SchedulePage.groovy
+++ test/functional/frontlinesms/legal/functionaltests/pages/schedule/SchedulePage.groovy
@@ -6,9 +6,31 @@ class SchedulePage extends Page {
     static at = { $("title").text() == "Schedule" }
     static url = "schedule"
     static content = {
-        events { $("span[class='fc-event-title']") }
-        deleteEventButton { $('#delete-event') }
-        popupDialogVisiblity { $('#view-event').displayed }
+        events {
+            try {
+                $("span[class='fc-event-title']")
+            }
+            catch(Exception e)
+            {
+                null
+            }
+        }
+        isEventListSize {
+            try {
+                $("span[class='fc-event-title']").size()
+
+            }
+            catch(Exception e)
+            {
+                0
+            }
+        }
+        deleteEvent {
+            $('#delete-event').click()
+           $('#cancel-confirm-yes').click()
+           true
+        }
         popupDialogEventTitle {$('#event-title').text()}
+
     }
 }
