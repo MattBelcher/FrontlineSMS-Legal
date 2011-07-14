671f4087eaec661c860ec719661cd36aca2b456b
Devaradhan <devaradhan@gmail.com>
2011-07-14 11:33:16 +0530

    matt/deva: fixed bug in create event page -should not display cancel confirm dialog if all the fields are empty

--- src/javascript/eventInteractions.js
+++ src/javascript/eventInteractions.js
@@ -2,39 +2,70 @@ var frontlinesms = this.frontlinesms || {};
 
 frontlinesms.activateDatePicker = function() {
     $('#event-date').datepicker({
-        changeMonth: true,
-        changeYear: true,
-        dateFormat: 'MM d, yy'
-    });
+                changeMonth: true,
+                changeYear: true,
+                dateFormat: 'MM d, yy'
+            });
 
 }
 frontlinesms.activateTimePicker = function() {
     $('#event-start-time').timeEntry({
-        spinnerImage: ''
-    });
+                spinnerImage: ''
+            });
     $('#event-end-time').timeEntry({
-        spinnerImage: ''
-    });
+                spinnerImage: ''
+            });
+}
+
+frontlinesms.redirectToHomePage = function() {
+    $(window.location).attr("href", "/");
 }
 
 frontlinesms.eventCancelConfirmAction = function() {
-    $("#event-cancel-dialog").dialog({
-        modal: true,
-        buttons: [
-            {
-                text: "Yes, Cancel This Event",
-                click: function() {
-                    $(window.location).attr("href", "/");
-                },
-                id: "cancel-confirm-yes"
-            },
-            {
-                text: "No, Don't Cancel This Event",
-                click: function() {
-                    $(this).dialog("close")
-                },
-                id: "cancel-confirm-no"
-            }
-        ]
-    });
+    if (frontlinesms.isPageEmpty()) {
+        frontlinesms.redirectToHomePage
+    }
+    else {
+        $("#event-cancel-dialog").dialog({
+                    modal: true,
+                    buttons: [
+                        {
+                            text: "Yes, Cancel This Event",
+                            click: function() {
+                                $(window.location).attr("href", "/");
+                            },
+                            id: "cancel-confirm-yes"
+                        },
+                        {
+                            text: "No, Don't Cancel This Event",
+                            click: function() {
+                                $(this).dialog("close")
+                            },
+                            id: "cancel-confirm-no"
+                        }
+                    ]
+                });
+        $("#event-cancel-dialog").dialog('open');
+
+    }
+}
+
+frontlinesms.isPageEmpty = function() {
+    if (frontlinesms.elementIsEmpty($("#event-title")) && frontlinesms.elementIsEmpty($("#event-date")) &&
+            frontlinesms.elementIsEmpty($("#event-start-time")) && frontlinesms.elementIsEmpty($("#event-end-time"))) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+frontlinesms.elementIsEmpty = function(element) {
+    if ($(element).val() == "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
