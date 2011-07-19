02f17fa1ac014b26163289a4ef8982b9db931634
Afmrak <enmrak@gmail.com>
2011-07-19 11:00:30 +0530

    Matt & Mrak : Altering the way we construct the war file. Removing the ant-1.7.0.jar, to force it to use a later version.

--- scripts/Dist.groovy
+++ scripts/Dist.groovy
@@ -4,6 +4,7 @@ includeTargets << grailsScript("_GrailsPackage")
 def winstoneDir = "target/winstone"
 def winstoneManifest = "${winstoneDir}/META-INF/MANIFEST.MF"
 def warFileName = "target/${metadata["app.name"]}-${metadata["app.version"]}.war"
+def unzippedWarDir = "target/unzippedWar"
 target(dist: "Packages the final distribution file") {
 
     ant.exec(executable: "grails") {
@@ -11,7 +12,10 @@ target(dist: "Packages the final distribution file") {
         arg(value: "war")
         arg(value: warFileName)
     }
-
+    ant.unzip(src: warFileName, dest:unzippedWarDir)
+    ant.delete(file:"${unzippedWarDir}/WEB-INF/lib/ant-1.7.0.jar")
+    ant.delete(file:"${unzippedWarDir}/WEB-INF/lib/ant-launcher-1.7.0.jar")
+    ant.zip(basedir:unzippedWarDir, destFile: warFileName)
     ant.mkdir(dir: winstoneDir)
     ant.unzip(src: "lib/java/winstone-0.8.1.jar", dest: winstoneDir)
     ant.copy(file: warFileName, tofile: "${winstoneDir}/embedded.war")
